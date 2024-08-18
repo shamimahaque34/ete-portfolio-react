68488479edd5b1db4072b5a875f7c11c21c20710
Shamima Haque <haqueshamima34@gmail.com>
2024-08-18 23:32:04 +0600

    contact page complete

--- src/componenets/contact/Contact.jsx
+++ src/componenets/contact/Contact.jsx
@@ -1,5 +1,40 @@
 export default function Contact(){
     return(
-        
+        <div  className="container">
+        <div  className="row">
+          <div  className="col-md-6 details-left connect-info">
+            <div  className="info-item">
+              <strong>Phone</strong>
+              <p  className="mb-0">+880 1313 924 759</p>
+            </div>
+  
+            <div  className="info-item">
+              <strong>Email</strong>
+              <p  className="mb-0">haqueshamima34@gmail.com</p>
+            </div>
+  
+            <div  className="info-item">
+              <strong>Address</strong>
+              <p  className="mb-0">68/8-B,Zigatola,Dhanmondi,Dhaka-1209,Bangladesh</p>
+            </div>
+          </div>
+  
+          <div  className="col-md-6">
+            <form  className="contact-form">
+              <input type="text" name="name" placeholder="Your name" />
+              <input type="text" name="email" placeholder="Your email" />
+              <textarea
+                name="message"
+                rows="4"
+                placeholder="Your message"
+              ></textarea>
+  
+              <button type="submit" name="send"  className="main-button">
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     )
 }
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,12 @@
+import PageTitle from "../../componenets/contact/PageTitle";
+import Contacts from "../../componenets/contact/Contact";
+import Map from "../../componenets/contact/Map";
 export default function Contact(){
     return(
-        <h3>This is contact page</h3>
+        <>
+        <PageTitle></PageTitle>
+        <Contacts></Contacts>
+        <Map></Map>
+        </>
     )
 }
