b831906c62b8daa3a997dc4a802501367fb982bb
Shamima Haque <haqueshamima34@gmail.com>
2024-08-19 12:43:11 +0600

    api data from laravel is coming using axios

--- src/componenets/portfolio/Testimonial.jsx
+++ src/componenets/portfolio/Testimonial.jsx
@@ -1,66 +1,55 @@
+import axios from 'axios';
+import { useState ,useEffect } from 'react';
+
 export default function Testimonial(){
+    const [info,setInfo] =useState([]);
+
+  const fetchData = () =>{
+    return axios.get("https://admin.shamima.xyz/api/testimonial-info").then((response) =>{
+        setInfo(response.data)});
+   
+  }
+  
+    useEffect(() =>{
+      fetchData();
+    },[])
+
     return(
-        <div    className="container section-padding">
-        <div    className="row">
-            <div    className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                <div id="testimonial-slider"    className="owl-carousel">
-                    <div    className="testimonial">
-                        <div    className="client">
-                            <img src="assets/images/profile.jpeg" alt=""/>
-                        </div>
-                        <div    className="testimonial-content">
-                            <p    className="description">
-                              I am a Web Developer, based in Dhaka, Bangladesh. I did my bachelor
-                              in Computer Science and Engineering. I also underwent several
-                              trainings in Web Development at various renowned training
-                              institutions. I am expert in HTML/HTML5, CSS/CSS3, Bootstrap 4, Tailwind CSS,
-                              Wordpress Theme Customization, Wordpress Theme Development, JavaScript, Php, Laravel, Vue Js,React Js and more.
-                            </p>
-                            <h3    className="client-title">Shamima
-                                <small    className="post">Web Developer</small>
-                            </h3>
-                        </div>
+        <>
+       
+        <div className="container section-padding">
+         <div className="row">
+        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        
+            
+            <div id=""  className="">
+           
+              
+            {info.map((infoObj)=>(
+                <div className="testimonial"key={infoObj.id}>
+                
+                    <div className="client" >
+                   
+                        <img src={infoObj.image} alt="" />
                     </div>
-     
-                    <div    className="testimonial">
-                        <div    className="client">
-                            <img src="assets/images/profile.jpeg" alt=""/>
-                        </div>
-                        <div    className="testimonial-content">
-                            <p    className="description">
-                              I am a Web Developer, based in Dhaka, Bangladesh. I did my bachelor
-                              in Computer Science and Engineering. I also underwent several
-                              trainings in Web Development at various renowned training
-                              institutions. I am expert in HTML/HTML5, CSS/CSS3, Bootstrap 4, Tailwind CSS,
-                              Wordpress Theme Customization, Wordpress Theme Development, JavaScript, Php, Laravel, Vue Js and more.
-                            </p>
-                            <h3    className="client-title">Shamima
-                                <small    className="post">Web Developer</small>
-                            </h3>
-                        </div>
+                    <div className="testimonial-content">
+                        <p  className="description">
+                          {infoObj.description}
+                        </p>
+                        <h3    className="client-title">{infoObj.name}
+                            <small className="post">{infoObj.designation}</small>
+                        </h3>
                     </div>
                     
-                    <div    className="testimonial">
-                        <div    className="client">
-                            <img src="assets/images/profile.jpeg" alt=""/>
-                        </div>
-                        <div    className="testimonial-content">
-                            <p    className="description">
-                              I am a Web Developer, based in Dhaka, Bangladesh. I did my bachelor
-                              in Computer Science and Engineering. I also underwent several
-                              trainings in Web Development at various renowned training
-                              institutions. I am expert in HTML/HTML5, CSS/CSS3, Bootstrap 4,
-                              Wordpress Theme Customization, JavaScript, Php, Laravel and more.
-                            </p>
-                            <h3    className="client-title">Shamima
-                                <small    className="post">Web Developer</small>
-                            </h3>
-                        </div>
-                    </div>
-                    
+
                 </div>
-            </div>
+                ))}
+                </div>
+         
         </div>
-    </div> 
-    )
+    </div>
+</div> 
+
+
+   </> );
 }
